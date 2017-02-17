a03acbd4fd55376a87e88ed16b58515507c18087
吴佳 <web@wujiabk.com>
2017-02-17 22:13:35 +0800

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,31 @@
-$(function(){
+$(function() {
     $(".user-img").delay(500).animate({
-        top:0
-    },1500,'easeOutExpo');
+        top: 0
+    }, 1500, 'easeOutExpo');
+    $(".user-message").find("p").delay(400).animate({
+        marginLeft: "0.35rem"
+    }, 1500, "easeOutExpo");
+
+
     $('#dowebok').fullpage({
         sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90'],
-        afterLoad: function(anchorLink, index){
+        afterLoad: function(anchorLink, index) {
             switch (index) {
                 case 1:
                     $(".user-img").delay(300).animate({
-                        top:"0"
-                    },1500,'easeOutExpo');
+                        top: "0"
+                    }, 1500, 'easeOutExpo');
+                    $(".user-message").find("p").delay(400).animate({
+                        marginLeft: "0.35rem"
+                    }, 1500, "easeOutExpo");
                     break;
                 case 2:
-
+                    $(".section2").find("h3").delay(300).animate({
+                        left: "0"
+                    }, 1500, "easeOutExpo");
+                    $(".gzjl").delay(800).animate({
+                        left: "0"
+                    }, 1500, "easeOutExpo");
                     break;
                 case 3:
 
@@ -22,14 +35,23 @@ $(function(){
                     break;
             };
         },
-        onLeave: function(index, direction){
+        onLeave: function(index, direction) {
             switch (index) {
                 case 1:
                     $(".user-img").delay(300).animate({
-                        top:"-3rem"
-                    },1500,'easeOutExpo');
+                        top: "-3rem"
+                    }, 1500, 'easeOutExpo');
+                    $(".user-message").find("p").delay(400).animate({
+                        marginLeft: "-100%"
+                    }, 1500, "easeOutExpo");
                     break;
                 case 2:
+                    $(".section2").find("h3").delay(300).animate({
+                        left: "100%"
+                    }, 1500, "easeOutExpo");
+                    $(".gzjl").delay(300).animate({
+                        left: "100%"
+                    }, 1500, "easeOutExpo");
 
                     break;
                 case 3:
